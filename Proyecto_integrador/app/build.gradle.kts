2c1c4072d2cd3478dad0ded136dca077ed1b86c1
williampostillos <william.postillos@tecsup.edu.pe>
2023-10-19 02:07:45 -0500

    Se agrego la vista registro y se modifico la vista login

--- Proyecto_integrador/app/build.gradle.kts
+++ Proyecto_integrador/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,6 +47,9 @@ dependencies {
 
     //API SplashScreen
     implementation ("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
 
 
     testImplementation("junit:junit:4.13.2")
--- Proyecto_integrador/app/src/main/java/com/example/proyecto_integrador/RegistroActivity.kt
+++ Proyecto_integrador/app/src/main/java/com/example/proyecto_integrador/RegistroActivity.kt
@@ -1,11 +1,39 @@
 package com.example.proyecto_integrador
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class RegistroActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        val txt_registro: TextView = findViewById(R.id.txt_registro)
+        val btn_iniciarSesion: Button = findViewById(R.id.btn_iniciarSesion)
+
+
+        txt_registro.setOnClickListener {
+            home()
+        }
+
+        btn_iniciarSesion.setOnClickListener{
+            login()
+        }
+
+    }
+    private fun home(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun login(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
